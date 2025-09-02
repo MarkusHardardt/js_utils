ac29cd3b5f9d25de71275670d5b02b9659b31480
Markus Hardardt <36100539+MarkusHardardt@users.noreply.github.com>
2025-09-02 18:23:20 +0200

    Added task

---
+++ index.js
@@ -0,0 +1,16 @@
+(function(root) {
+  "use strict";
+
+  var exp = {
+    task: typeof module !== "undefined" && module.exports ? require('./src/task').js : root.other
+  };
+
+  Object.seal(exp);
+
+  // export
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = exp;
+  } else {
+    root.utils = exp;
+  }
+}(globalThis));
