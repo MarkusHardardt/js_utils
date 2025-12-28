e17b775be7e1fa4d47f8889c234d0731e9c9c65e
Markus Hardardt <markus.hardardt@gmx.ch>
2025-12-28 14:55:41 +0100

    Extended analyser

--- analyse_dependencies.js
+++ analyse_dependencies.js
@@ -1,6 +1,11 @@
 (function () {
+    // output files
+    const result_dependencies_json = './analyse_dependencies_result.json';
+    const source_code_txt = './analyse_dependencies_source.txt';
+
     const fs = require('fs');
     const Regex = require('./src/Regex.js');
+    const Core = require('./src/Core.js');
     const moduleNameRegex = /^([a-zA-Z][_a-zA-Z0-9]*)\.js$/;
     const moduleRegex = /\bconst\s+([a-zA-Z][_a-zA-Z0-9]*)\s*=\s*isNodeJS\s*\?\s*require\s*\(\s*'\.\/\1\.js'\s*\)\s*:\s*root\s*\.\s*\1\s*;/g;
     fs.readdir('./src', (error, files) => {
@@ -19,7 +24,8 @@
             const text = fs.readFileSync(`./src/${file}`, 'utf8');
             Regex.each(moduleRegex, text, (start, end, match) => dependencies[moduleName].push(match[1]), true);
         }
-        fs.writeFileSync('./dependencies.json', JSON.stringify(dependencies, undefined, 2), 'utf8');
-        console.log(`done:\n${JSON.stringify(dependencies, undefined, 2)}`)
+        fs.writeFileSync(result_dependencies_json, JSON.stringify(dependencies, undefined, 2), 'utf8');
+        fs.writeFileSync(source_code_txt, Core.generateLibraryFileAccess(dependencies), 'utf8');
+        console.log('done');
     });
 }());
