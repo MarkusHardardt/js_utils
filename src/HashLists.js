bec8033c69c9962f0e433d0cfb6d41999301088c
Markus Hardardt <markus.hardardt@gmx.ch>
2025-12-29 19:08:29 +0100

    Refactored

--- src/HashLists.js
+++ src/HashLists.js
@@ -1,8 +1,4 @@
 /**
- * Copyright (c) 2018, Markus Hardardt
- */
-
-/**
  * HashLists
  * 
  * HashLists is a JavaScript implementation of a provider for three different
@@ -19,147 +15,148 @@
     const Utilities = isNodeJS ? require('./Utilities') : root.Utilities;
 
     // our mode constants:
-    var IDENTICAL_VALUES_PER_KEY = 1;
-    var NO_IDENTICAL_VALUES_PER_KEY = 2;
-    var NO_EQUAL_VALUES_PER_KEY = 3;
+    const Mode = Object.freeze({
+        IdenticalValuesPerKey: 1,
+        NoIdenticalValuesPerKey: 2,
+        NoEqualValuesPerKey: 3
+    });
+    HashLists.Mode = Mode;
 
     // constructor for internal list structure we need only to decide if a value
     // is an array or our internal storage container
-    function List() {
-        // call super constructor
-        Array.call(this);
+    class List {
+        constructor() {
+            // call super constructor
+            Array.call(this);
+        }
     }
-    // our list inherit from arrays
-    List.prototype = Object.create(Array.prototype);
-    List.prototype.constructor = List;
 
     // constructor for our hash lists implementations
-    function Impl(i_mode) {
-        // this makes sure we have one ouf our supported modes
-        switch (i_mode) {
-            case NO_IDENTICAL_VALUES_PER_KEY:
-                this._mode = NO_IDENTICAL_VALUES_PER_KEY;
-                break;
-            case NO_EQUAL_VALUES_PER_KEY:
-                this._mode = NO_EQUAL_VALUES_PER_KEY;
-                break;
-            case IDENTICAL_VALUES_PER_KEY:
-            default:
-                this._mode = IDENTICAL_VALUES_PER_KEY;
-                break;
+    class Impl {
+        constructor(mode) {
+            // this makes sure we have one ouf our supported modes
+            switch (mode) {
+                case Mode.NoIdenticalValuesPerKey:
+                    this._mode = Mode.NoIdenticalValuesPerKey;
+                    break;
+                case Mode.NoEqualValuesPerKey:
+                    this._mode = Mode.NoEqualValuesPerKey;
+                    break;
+                case Mode.IdenticalValuesPerKey:
+                default:
+                    this._mode = Mode.IdenticalValuesPerKey;
+                    break;
+            }
+            // plain javascript objects are implemented as associative arrays - so we
+            // use this data object for our internal data storage
+            this._data = {};
+            this._size = 0;
+            this._selection = undefined;
         }
-        // plain javascript objects are implemented as associative arrays - so we
-        // use this data object for our internal data storage
-        this._data = {};
-        this._size = 0;
-        this._selection = undefined;
-    };
-
-    Impl.prototype = {
         /**
          * Add an entry to the list
-         * 
+         *
          * @param {Object}
-         *          i_key The key
+         *          key The key
          * @param {Object}
-         *          i_value The value
+         *          value The value
          */
-        add: function (i_key, i_value) {
-            if (typeof i_key === 'string') {
-                var data = this._data, object = data[i_key];
+        add(key, value) {
+            if (typeof key === 'string') {
+                var data = this._data, object = data[key];
                 if (object === undefined) {
-                    data[i_key] = i_value;
+                    data[key] = value;
                     this._size++;
                     return true;
                 }
                 else if (List.prototype.isPrototypeOf(object)) {
                     switch (this._mode) {
-                        case NO_IDENTICAL_VALUES_PER_KEY:
+                        case Mode.NoIdenticalValuesPerKey:
                             for (var i = 0, l = object.length; i < l; i++) {
-                                if (object[i] === i_value) {
+                                if (object[i] === value) {
                                     return false;
                                 }
                             }
                             break;
-                        case NO_EQUAL_VALUES_PER_KEY:
+                        case Mode.NoEqualValuesPerKey:
                             for (var i = 0, l = object.length; i < l; i++) {
-                                if (object[i] == i_value) {
+                                if (object[i] == value) {
                                     return false;
                                 }
                             }
                             break;
-                        case IDENTICAL_VALUES_PER_KEY:
+                        case Mode.IdenticalValuesPerKey:
                         default:
                             break;
                     }
-                    object.push(i_value);
+                    object.push(value);
                     return true;
                 }
                 else {
                     switch (this._mode) {
-                        case NO_IDENTICAL_VALUES_PER_KEY:
-                            if (object === i_value) {
+                        case Mode.NoIdenticalValuesPerKey:
+                            if (object === value) {
                                 return false;
                             }
                             break;
-                        case NO_EQUAL_VALUES_PER_KEY:
-                            if (object == i_value) {
+                        case Mode.NoEqualValuesPerKey:
+                            if (object == value) {
                                 return false;
                             }
                             break;
-                        case IDENTICAL_VALUES_PER_KEY:
+                        case Mode.IdenticalValuesPerKey:
                         default:
                             break;
                     }
                     var list = new List();
                     list.push(object);
-                    list.push(i_value);
-                    data[i_key] = list;
+                    list.push(value);
+                    data[key] = list;
                     return true;
                 }
             }
             else {
                 return false;
             }
-        },
-        put: function (i_key, i_value) {
-            this.add(i_key, i_value);
-        },
+        }
+        put(key, value) {
+            this.add(key, value);
+        }
         /**
          * Get the keys
          */
-        keys: function () {
+        keys() {
             return Utilities.getObjectProperties(this._data);
-        },
-        remove: function (i_key, i_value) {
-            if (typeof i_key === 'string') {
-                var data = this._data, object = data[i_key];
+        }
+        remove(key, value) {
+            if (typeof key === 'string') {
+                var data = this._data, object = data[key];
                 if (object === undefined) {
                     return false;
                 }
                 else if (List.prototype.isPrototypeOf(object)) {
                     switch (this._mode) {
-                        case NO_IDENTICAL_VALUES_PER_KEY:
+                        case Mode.NoIdenticalValuesPerKey:
                             for (var i = 0, l = object.length; i < l; i++) {
-                                if (object[i] === i_value) {
+                                if (object[i] === value) {
                                     object.splice(i, 1);
                                     if (object.length === 0) {
-                                        delete data[i_key];
+                                        delete data[key];
                                         this._size--;
                                     }
                                     return true;
                                 }
                             }
                             return false;
-                        case NO_EQUAL_VALUES_PER_KEY:
-                        case IDENTICAL_VALUES_PER_KEY:
+                        case Mode.NoEqualValuesPerKey:
+                        case Mode.IdenticalValuesPerKey:
                         default:
                             for (var i = 0, l = object.length; i < l; i++) {
-                                var value = object[i];
-                                if (value == i_value) {
+                                var val = object[i];
+                                if (val == value) {
                                     object.splice(i, 1);
                                     if (object.length === 0) {
-                                        delete data[i_key];
+                                        delete data[key];
                                         this._size--;
                                     }
                                     return true;
@@ -170,20 +167,20 @@
                 }
                 else {
                     switch (this._mode) {
-                        case NO_IDENTICAL_VALUES_PER_KEY:
-                            if (object === i_value) {
-                                delete data[i_key];
+                        case Mode.NoIdenticalValuesPerKey:
+                            if (object === value) {
+                                delete data[key];
                                 this._size--;
                                 return true;
                             }
                             else {
                                 return false;
                             }
-                        case NO_EQUAL_VALUES_PER_KEY:
-                        case IDENTICAL_VALUES_PER_KEY:
+                        case Mode.NoEqualValuesPerKey:
+                        case Mode.IdenticalValuesPerKey:
                         default:
-                            if (object == i_value) {
-                                delete data[i_key];
+                            if (object == value) {
+                                delete data[key];
                                 this._size--;
                                 return true;
                             }
@@ -196,14 +193,14 @@
             else {
                 return false;
             }
-        },
-        clear: function () {
+        }
+        clear() {
             this._data = {};
             this._size = 0;
-        },
-        size: function (i_key) {
-            if (typeof i_key === 'string') {
-                var data = this._data, object = data[i_key];
+        }
+        size(key) {
+            if (typeof key === 'string') {
+                var data = this._data, object = data[key];
                 if (object === undefined) {
                     return 0;
                 }
@@ -217,22 +214,22 @@
             else {
                 return this._size;
             }
-        },
-        getValues: function (i_key, i_collection) {
-            if (typeof i_key === 'string') {
-                var data = this._data, object = data[i_key];
+        }
+        getValues(key, collection) {
+            if (typeof key === 'string') {
+                var data = this._data, object = data[key];
                 if (object === undefined) {
                     return false;
                 }
                 else if (List.prototype.isPrototypeOf(object)) {
-                    var result = i_collection ? i_collection : [];
+                    var result = collection ? collection : [];
                     for (var i = 0, l = object.length; i < l; i++) {
                         result.push(object[i]);
                     }
                     return result;
                 }
                 else {
-                    var result = i_collection ? i_collection : [];
+                    var result = collection ? collection : [];
                     result.push(object);
                     return result;
                 }
@@ -240,31 +237,31 @@
             else {
                 return false;
             }
-        },
-        containsKey: function (i_key) {
-            return this._data[i_key] !== undefined;
-        },
-        containsValue: function (i_key, i_value) {
-            if (typeof i_key === 'string') {
-                var data = this._data, object = data[i_key];
+        }
+        containsKey(key) {
+            return this._data[key] !== undefined;
+        }
+        containsValue(key, value) {
+            if (typeof key === 'string') {
+                var data = this._data, object = data[key];
                 if (object === undefined) {
                     return false;
                 }
                 else if (List.prototype.isPrototypeOf(object)) {
                     switch (this._mode) {
-                        case NO_IDENTICAL_VALUES_PER_KEY:
+                        case Mode.NoIdenticalValuesPerKey:
                             for (var i = 0, l = object.length; i < l; i++) {
-                                if (object[i] === i_value) {
+                                if (object[i] === value) {
                                     return true;
                                 }
                             }
                             return false;
-                        case NO_EQUAL_VALUES_PER_KEY:
-                        case IDENTICAL_VALUES_PER_KEY:
+                        case Mode.NoEqualValuesPerKey:
+                        case Mode.IdenticalValuesPerKey:
                         default:
                             for (var i = 0, l = object.length; i < l; i++) {
-                                var value = object[i];
-                                if (value == i_value) {
+                                var val = object[i];
+                                if (val == value) {
                                     return true;
                                 }
                             }
@@ -273,22 +270,22 @@
                 }
                 else {
                     switch (this._mode) {
-                        case NO_IDENTICAL_VALUES_PER_KEY:
-                            return object === i_value;
-                        case NO_EQUAL_VALUES_PER_KEY:
-                        case IDENTICAL_VALUES_PER_KEY:
+                        case Mode.NoIdenticalValuesPerKey:
+                            return object === value;
+                        case Mode.NoEqualValuesPerKey:
+                        case Mode.IdenticalValuesPerKey:
                         default:
-                            return object == i_value;
+                            return object == value;
                     }
                 }
             }
             else {
                 return false;
             }
-        },
-        selectValue: function (i_key) {
-            if (typeof i_key === 'string') {
-                var data = this._data, object = data[i_key];
+        }
+        selectValue(key) {
+            if (typeof key === 'string') {
+                var data = this._data, object = data[key];
                 if (object !== undefined) {
                     this._selection = object;
                     return true;
@@ -301,8 +298,8 @@
             else {
                 return false;
             }
-        },
-        getSelectedValueCount: function () {
+        }
+        getSelectedValueCount() {
             var selection = this._selection;
             if (selection !== undefined) {
                 return List.prototype.isPrototypeOf(selection) ? selection.length : 1;
@@ -310,11 +307,11 @@
             else {
                 return -1;
             }
-        },
-        getSelectedValue: function (i_index) {
+        }
+        getSelectedValue(index) {
             var selection = this._selection;
             if (selection !== undefined) {
-                return List.prototype.isPrototypeOf(selection) ? selection[i_index] : (i_index === 0 ? selection : undefined);
+                return List.prototype.isPrototypeOf(selection) ? selection[index] : (index === 0 ? selection : undefined);
             }
             else {
                 return undefined;
@@ -323,25 +320,25 @@
     }
 
     // for historical reasons we need a constructor for each of our three types
-    function IdenticalValuesPerKey() {
-        Impl.call(this, IDENTICAL_VALUES_PER_KEY);
+    class IdenticalValuesPerKey extends Impl {
+        constructor() {
+            super(Mode.IdenticalValuesPerKey);
+        }
     }
-    IdenticalValuesPerKey.prototype = Object.create(Impl.prototype);
-    IdenticalValuesPerKey.prototype.constructor = IdenticalValuesPerKey;
     HashLists.IdenticalValuesPerKey = IdenticalValuesPerKey;
 
-    function NoIdenticalValuesPerKey() {
-        Impl.call(this, NO_IDENTICAL_VALUES_PER_KEY);
+    class NoIdenticalValuesPerKey extends Impl {
+        constructor() {
+            super(Mode.NoIdenticalValuesPerKey);
+        }
     }
-    NoIdenticalValuesPerKey.prototype = Object.create(Impl.prototype);
-    NoIdenticalValuesPerKey.prototype.constructor = NoIdenticalValuesPerKey;
     HashLists.NoIdenticalValuesPerKey = NoIdenticalValuesPerKey;
 
-    function NoEqualValuesPerKey() {
-        Impl.call(this, NO_EQUAL_VALUES_PER_KEY);
+    class NoEqualValuesPerKey extends Impl {
+        constructor() {
+            super(Mode.NoEqualValuesPerKey);
+        }
     }
-    NoEqualValuesPerKey.prototype = Object.create(Impl.prototype);
-    NoEqualValuesPerKey.prototype.constructor = NoEqualValuesPerKey;
     HashLists.NoEqualValuesPerKey = NoEqualValuesPerKey;
 
     Object.freeze(HashLists);
