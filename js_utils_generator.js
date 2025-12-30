bdc6569be1028fb3a882de8df7615bc0df3089b4
Markus Hardardt <markus.hardardt@gmx.ch>
2025-12-30 19:32:57 +0100

    Working on generator

--- js_utils_generator.js
+++ js_utils_generator.js
@@ -1,5 +1,63 @@
 (function () {
-    require('../src/Core.js').analyseLibrary('./src', '../js_utils_dependencies.js', './js_utils.js');
+    const isNodeJS = typeof require === 'function';
+    const Executor = isNodeJS ? require('./src/Executor.js') : undefined;
+    const Core = isNodeJS ? require('./src/Core.js') : undefined;
+    const Helper = isNodeJS ? require('./env/Helper.js') : undefined;
+
+    function generate(options) {
+        const tasks = [];
+        let dependencies, topologicalSortedComponents, index_js;
+        // load dependencies as tree object
+        tasks.push((onSuccess, onError) => {
+            Helper.loadDependencies(options.directory, options.ignorables, result => {
+                dependencies = result;
+                onSuccess();
+            }, onError)
+        });
+        tasks.push((onSuccess, onError) => {
+            try {
+                topologicalSortedComponents = Core.getTopologicalSorting(dependencies);
+                onSuccess();
+            } catch (error) {
+                onError(error);
+            }
+        });
+        tasks.push((onSuccess, onError) => {
+            index = Helper.generateIndexJs(options.name, options.scope, topologicalSortedComponents, options.browserIgnorables);
+            console.log(index);
+        });
+        tasks.push((onSuccess, onError) => {
+
+        });
+        tasks.push((onSuccess, onError) => {
+
+        });
+        tasks.push((onSuccess, onError) => {
+
+        });
+        tasks.push((onSuccess, onError) => {
+
+        });
+        tasks.push((onSuccess, onError) => {
+
+        });
+        tasks.push((onSuccess, onError) => {
+
+        });
+
+        Executor.run(tasks, () => console.log('server started successfully'), error => console.error(error));
+
+        //require('./src/Core.js').analyseLibrary('./src', '../js_utils_dependencies.js', './js_utils.js');
+    }
+
+    generate({
+        name: 'js_utils',
+        scope: '@markus.hardardt/',
+        directory: './src',
+        ignorables: ['EmptyTemplate'],
+        browserIgnorables: ['Server', 'WebServer', 'EmptyTemplate']
+    });
+
 
     // Edited manually 2025-12-27
     const js_utils_dependencies = {
--- src/Common.js
+++ src/Common.js
@@ -6,70 +6,64 @@
 
     /*  Observable inferface  */
     function validateAsObservable(instance, validateMethodArguments) {
-        Core.validateAs('Observable', instance, [
+        return Core.validateAs('Observable', instance, [
             'Subscribe(onRefresh)',
             'Unsubscribe(onRefresh)',
         ], validateMethodArguments);
-        return instance;
     }
     Common.validateAsObservable = validateAsObservable;
 
     /*  OperationalState interface  */
     function validateAsOperationalState(instance, validateMethodArguments) {
-        Core.validateAs('OperationalState', instance, [
+        return Core.validateAs('OperationalState', instance, [
             'IsOperational:boolean', // property getter returns true if operational
             'SubscribeOperationalState(onOperationalStateChanged)',
             'UnsubscribeOperationalState(onOperationalStateChanged)'
         ], validateMethodArguments);
-        return instance;
     }
     Common.validateAsOperationalState = validateAsOperationalState;
 
     /*  DataAccessObject inferface  */
     function validateAsDataAccessObject(instance, validateMethodArguments) {
         validateAsOperationalState(instance, validateMethodArguments);
-        Core.validateAs('DataAccessObject', instance, [
+        return Core.validateAs('DataAccessObject', instance, [
             'GetType(dataId)',
             'SubscribeData(dataId, onRefresh)',
             'UnsubscribeData(dataId, onRefresh)',
             'Read(dataId, onResponse, onError)',
             'Write(dataId, value)'
         ], validateMethodArguments);
-        return instance;
     }
     Common.validateAsDataAccessObject = validateAsDataAccessObject;
 
     /*  Connection inferface  */
     function validateAsConnection(instance, validateMethodArguments) {
-        Core.validateAs('Connection', instance, [
+        return Core.validateAs('Connection', instance, [
             'Ping(onResponse, onError)',
             'Register(receiver, handler)',
             'Unregister(receiver)',
             'Send(receiver, data, onResponse, onError)'
         ], validateMethodArguments);
-        return instance;
     }
     Common.validateAsConnection = validateAsConnection;
 
     /*  ClientConnector inferface */
     function validateAsClientConnector(instance, validateMethodArguments) {
-        Core.validateAs('ClientConnector', instance, [
+        return Core.validateAs('ClientConnector', instance, [
             'OnOpen()',
             'OnClose()'
         ], validateMethodArguments);
-        return instance;
     }
     Common.validateAsClientConnector = validateAsClientConnector;
 
     /*  ServerConnector inferface  */
     function validateAsServerConnector(instance, validateMethodArguments) {
-        Core.validateAs('ServerConnector', instance, [
+        return Core.validateAs('ServerConnector', instance, [
             'OnOpen()',
             'OnReopen()',
             'OnClose()',
             'OnDispose()'
         ], validateMethodArguments);
-        return instance;
     }
     Common.validateAsServerConnector = validateAsServerConnector;
 
