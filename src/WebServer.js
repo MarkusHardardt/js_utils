e3ed1436b90d760663a010e8c4c2c047e4c1dee8
Markus Hardardt <markus.hardardt@gmx.ch>
2026-02-25 18:34:53 +0100

    added feature to register/unregister post requests dynamically at runtime

--- src/WebServer.js
+++ src/WebServer.js
@@ -7,6 +7,7 @@
         throw new Error('WebServer is not available on client');
     }
 
+    const JsonFX = isNodeJS ? require('./JsonFX.js') : root.JsonFX;
     const Server = isNodeJS ? require('./Server.js') : root.Server;
     const js_rx = /\.js$/i;
     const css_rx = /\.css$/i;
@@ -27,12 +28,14 @@
         #server;
         #app;
         #random_id;
+        #postRequestHandler;
         constructor(options = {}) {
             this.#scripts = [];
             this.#styles = [];
             this.#paths = {};
             this.#title = '';
             this.#body = '';
+            this.#postRequestHandler = {};
             const app = this.#app = express();
             this.#secure = typeof options.secureKeyFile === 'string' && typeof options.secureCertFile === 'string';
             this.#server = this.#secure ? https.createServer({
@@ -59,22 +62,42 @@
             });
             // this returns our main html document
             app.get('/', (req, res) => res.send(this.#generate_html()));
+            // handle post requests
+            if (typeof options.postRequestUrl === 'string' && options.postRequestUrl.length > 0) {
+                app.post(options.postRequestUrl, (request, response) => {
+                    const body = request.body;
+                    if (typeof body.receiver !== 'string') {
+                        response.send(JsonFX.stringify({ error: `Invalid receiver for request: ${body.receiver}` }, false));
+                    } else {
+                        const handler = this.#postRequestHandler[body.receiver];
+                        if (typeof handler !== 'function') {
+                            response.send(JsonFX.stringify({ error: `Invalid handler for receiver: ${body.receiver}` }, false));
+                            return;
+                        }
+                        try {
+                            handler(
+                                body.request,
+                                result => response.send(JsonFX.stringify({ result }, false)),
+                                error => response.send(JsonFX.stringify({ error: error.toString() }, false))
+                            );
+                        } catch (error) {
+                            response.send(JsonFX.stringify({ error: `Failed calling handler for receiver: ${body.receiver}: ${error.message}` }, false));
+                        }
+                    }
+                });
+            }
         }
-
         get isSecure() {
             return this.#secure;
         }
-
         prepareFavicon(path) {
             // gimp: "./src/app/favicon.xcf"
             // alternative url: "https://www.favicon-generator.org/"
             this.#app.use('/favicon.ico', express.static(path));
         }
-
         set randomFileIdEnabled(value) {
             this.#random_id = value === true;
         }
-
         addStaticDirectory(directory, id) {
             if (typeof directory === 'string') {
                 if (typeof id === 'string') {
@@ -94,12 +117,10 @@
                 throw new Error('Invalid directory string: ' + directory);
             }
         }
-
         #prepare(directory, name, buffer) {
             const id = this.addStaticDirectory(directory);
             buffer.push(`/${id}/${name}`);
         }
-
         addStaticFile() { // Note: No not change to lambda function, because 'arguments' will not work anymore!
             const dir = arguments.length === 1 ? path.dirname(arguments[0]) : arguments[0];
             const name = arguments.length === 1 ? path.basename(arguments[0]) : arguments[1];
@@ -112,29 +133,23 @@
                 }
             }
         }
-
         post(url, onResponse) {
             this.#app.post(url, onResponse);
         }
-
         get(url, onResponse) {
             this.#app.get(url, onResponse);
         }
-
         setTitle(title) {
             this.#title = title;
         }
-
         setBody(body) {
             this.#body = body;
         }
-
         clear() {
             this.#scripts.splice(0, this.#scripts.length);
             this.#styles.splice(0, this.#styles.length);
             this.#paths = {};
         }
-
         #generate_html() {
             let i, l, html = '<!DOCTYPE HTML><html><head>';
             html += '<meta charset="UTF-8">';
@@ -164,10 +179,33 @@
             html += '</body></html>';
             return html;
         }
-
         listen(port, onResponse) {
             // this._app.listen(port, onResponse);
             this.#server.listen(port, onResponse);
+        }
+        registerPostRequestHandler(receiver, onRequest) {
+            if (typeof receiver !== 'string') {
+                throw new Error(`WebServer.registerPostRequestHandler(): Invalid receiver for post request handler: '${receiver}'`);
+            } else if (typeof onRequest !== 'function') {
+                throw new Error(`WebServer.registerPostRequestHandler(): Post request handler for receiver '${receiver}' is not a function`);
+            } else if (this.#postRequestHandler[receiver] !== undefined) {
+                throw new Error(`WebServer.registerPostRequestHandler(): Post request handler '${receiver}' is already registered`);
+            } else {
+                this.#postRequestHandler[receiver] = onRequest;
+            }
+        }
+        unregisterPostRequestHandler(receiver, onRequest) {
+            if (typeof receiver !== 'string') {
+                throw new Error(`WebServer.unregisterPostRequestHandler(): Invalid receiver for post request handler: '${receiver}'`);
+            } else if (typeof onRequest !== 'function') {
+                throw new Error(`WebServer.unregisterPostRequestHandler(): Post request handler for receiver '${receiver}' is not a function`);
+            } else if (this.#postRequestHandler[receiver] === undefined) {
+                throw new Error(`WebServer.unregisterPostRequestHandler(): Post request handler '${receiver}' is not registered`);
+            } else if (this.#postRequestHandler[receiver] !== onRequest) {
+                throw new Error(`WebServer.unregisterPostRequestHandler(): Post request handler '${receiver}' is registered for another handler`);
+            } else {
+                delete this.#postRequestHandler[receiver];
+            }
         }
     }
     WebServer.Server = WebSrv;
