f2f3829be008d94268b15c49219bd42714c9e708
Markus Hardardt <markus.hardardt@gmx.ch>
2026-03-15 15:38:32 +0100

    cosmetics

--- src/Core.js
+++ src/Core.js
@@ -71,15 +71,20 @@
         const queue = [];
         const result = [];
         for (const node in dependencies) {
-            if (!inDegree.has(node))
-                inDegree.set(node, 0);
-            for (const dep of dependencies[node]) {
-                graph.set(dep, (graph.get(dep) || []).concat(node));
-                inDegree.set(node, (inDegree.get(node) || 0) + 1);
+            if (dependencies.hasOwnProperty(node)) {
+                if (!inDegree.has(node)) {
+                    inDegree.set(node, 0);
+                }
+                for (const dep of dependencies[node]) {
+                    graph.set(dep, (graph.get(dep) || []).concat(node));
+                    inDegree.set(node, (inDegree.get(node) || 0) + 1);
+                }
             }
         }
         for (const [node, degree] of inDegree.entries()) {
-            if (degree === 0) queue.push(node);
+            if (degree === 0) {
+                queue.push(node);
+            }
         }
         while (queue.length > 0) {
             const node = queue.shift();
@@ -92,7 +97,7 @@
             }
         }
         if (result.length !== inDegree.size) {
-            throw new Error("Cyclical dependency detected!");
+            throw new Error('Cyclical dependency detected!');
         }
         return result;
     }
